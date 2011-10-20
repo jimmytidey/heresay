94216c4d31f94bc7cd962afc698ec430076348d7
jimmytidey <jimmytidey@gmail.com>
2011-10-20 23:08:35 +0100

    improving help on the pin page

--- platform/heresay_ning_test.js
+++ platform/heresay_ning_test.js
@@ -58,8 +58,9 @@ heresay.addDiscussionLoction = function() {
 		"<input type='checkbox' checked='checked' id='location_possible'></p>"+
 		"<div id='toggle_content'><label for='location_name'>How would you refer to this location?</label>"+
 		"<input type='text' id='location_name' />";
-		
-	var mapHtml = '<div id="map_canvas" style="width:610px; height:325px; margin-top:20px"></div></div>';
+	
+	var mapHtml = '<p>Now drag the red marker to the location you want to refer to</p>' 	
+	mapHtml += '<div id="map_canvas" style="width:610px; height:325px; margin-top:20px"></div></div>';
 	
 	var selectHtml = "<label for='type' >Categories:</label><select id='type' style='margin-left:40px'>";
 	selectHtml += "<option value='select'>-select category-</option>";
