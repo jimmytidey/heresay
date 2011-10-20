5501fc2dd0500d2e716ef3d5c6275d8631633112
jimmytidey <jimmytidey@gmail.com>
2011-10-20 16:46:43 +0100

    adding a central switch on the main doamin

--- platform/heresay_ning_test.js
+++ platform/heresay_ning_test.js
@@ -476,12 +476,7 @@ $(document).ready(function() {
 
 heresay.addCookieSettings = function () {
 	
-	var yes_state ='';
-	var no_state ='';
-	
-	if (heresay.getCookie('heresay_harringay') == 'yes') {yes_state ='checked="checked"';}
-	
-	else {no_state ='checked="checked"';}
+
 	
 	html = '<fieldset class="nolegend" id="heresayButtons" >';
 	html += '<h3>Heresay Mapping Plugin </h3>';
@@ -496,31 +491,8 @@ heresay.addCookieSettings = function () {
 	jQuery('#heresayButtons').change(function(){
 		
 	}); 
-}
+}	
 
 
-heresay.getCookie = function (c_name)
-{
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-{
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
-    }
-  }
-}
-
-heresay.setCookie(c_name,value)
-{
-	var exdate=new Date();
-	var exdays =='600'; 
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie=c_name + "=" + c_value;
-}
 
 
---
+++ switch/index.php
@@ -0,0 +1,62 @@
+<? include('../header.php'); ?>
+	
+	<h1>Turn Heresay on and off</h1> 
+	
+	<form id='form'>
+		<fieldset class="nolegend" id="heresayButtons" >
+			<input type="radio" name="state" value="On"> On<br>
+			<input type="radio" name="state" value="Off"> Off<br>
+		</fieldset>
+	</form>	
+	
+	<script type="text/javascript">
+		
+		var yes_state ='';
+		var no_state ='';
+	
+		if (heresay.getCookie('heresay_harringay') == 'yes') {yes_state ='checked="checked"';}
+	
+		else {no_state ='checked="checked"';}
+			
+		//init if the cookie has been set 
+		if (getCookie('heresay_harringay') !== undefined) {
+			
+		}
+	
+		else {
+			setCookie('heresay_harringay', 'no');
+			alert('no cookie');
+		}		
+		
+		jQuery('#heresayButtons').change(function(){
+			alert('change');
+		});
+	
+		function getCookie(c_name)
+		{
+		var i,x,y,ARRcookies=document.cookie.split(";");
+		for (i=0;i<ARRcookies.length;i++)
+		{
+		  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		  x=x.replace(/^\s+|\s+$/g,"");
+		  if (x==c_name)
+		    {
+		    return unescape(y);
+		    }
+		  }
+		}
+
+		function setCookie(c_name,value)
+		{
+			var exdate=new Date();
+			var exdays =='600'; 
+			exdate.setDate(exdate.getDate() + exdays);
+			var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+			document.cookie=c_name + "=" + c_value;
+		}
+		
+
+	</script>
+	
+<?	include('../footer.php'); ?>
