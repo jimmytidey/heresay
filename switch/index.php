b4102458d451487c10bf73c1eae93fc806b68981
jimmytidey <jimmytidey@gmail.com>
2011-10-20 16:52:38 +0100

    adding a central switch on the main doamin

--- switch/index.php
+++ switch/index.php
@@ -34,22 +34,22 @@
 	
 		function getCookie(c_name)
 		{
-		var i,x,y,ARRcookies=document.cookie.split(";");
-		for (i=0;i<ARRcookies.length;i++)
-		{
-		  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-		  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-		  x=x.replace(/^\s+|\s+$/g,"");
-		  if (x==c_name)
-		    {
-		    return unescape(y);
-		    }
-		  }
+			var i,x,y,ARRcookies=document.cookie.split(";");
+			for (i=0;i<ARRcookies.length;i++)
+			{
+			  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+			  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+			  x=x.replace(/^\s+|\s+$/g,"");
+			  if (x==c_name)
+			    {
+			    	return unescape(y);
+			    }
+			}
 		}
 
 		function setCookie(c_name,value)
 		{
-			var exdate=new Date();
+			var exdate = new Date();
 			var exdays ='600'; 
 			exdate.setDate(exdate.getDate() + exdays);
 			var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
