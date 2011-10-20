ab8d88f2cd9da430b900b2a8de456927ff1b7480
jimmytidey <jimmytidey@gmail.com>
2011-10-20 15:47:30 +0100

    adding cookie detection

--- platform/heresay_ning_test.js
+++ platform/heresay_ning_test.js
@@ -455,9 +455,29 @@ heresay.getUrlVars = function() {
 }
 
 $(document).ready(function() {
-  heresay.init();	
+	 
+	if (getCookie('heresay_harringay') != "") {
+		heresay.init();
+	}
+
+	
 });
 
 
+
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+{
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
 
 
