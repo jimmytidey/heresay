66c42bf2e260694edfd88eb7b89d5cad3b066654
jimmytidey <jimmytidey@gmail.com>
2011-10-21 11:44:41 +0100

    reverting to setting that cookie locally

--- platform/heresay_ning_test.js
+++ platform/heresay_ning_test.js
@@ -509,7 +509,7 @@ heresay.getCookie = function (c_name)
 var i,x,y,ARRcookies=document.cookie.split(";");
 for (i=0;i<ARRcookies.length;i++)
 {
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOfOf("="));
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
   y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
   x=x.replace(/^\s+|\s+$/g,"");
   if (x==c_name)
