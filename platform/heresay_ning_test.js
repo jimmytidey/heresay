3fc90119de9d4dfabb6b11ba087d2184fe9a3fc3
jimmytidey <jimmytidey@gmail.com>
2011-10-21 13:12:33 +0100

    trying more robust cookie functions

--- platform/heresay_ning_test.js
+++ platform/heresay_ning_test.js
@@ -467,7 +467,7 @@ jQuery(document).ready(function() {
 
 	if (heresay.getCookie('heresay_harringay') === undefined) {
 		alert('resetting cookie')
-		heresay.setCookie('heresay_harringay', 'no');
+		heresay.setCookie('heresay_harringay', 'no', 30, '/', '', '' );
 	}
 
 	//put a control in for adding a cookie
@@ -504,37 +504,79 @@ heresay.addCookieSettings = function () {
 
 	jQuery('.heresaybtn').change(function(){
 		if ($('input:radio[name=heresaySetting]:checked').val() == 'On') {
-			heresay.setCookie('heresay_harringay', 'yes');
+			heresay.setCookie('heresay_harringay', 'yes', 30, '/', '', '');
 		}
 
-		else {heresay.setCookie('heresay_harringay', 'no');}
+		else {heresay.setCookie('heresay_harringay', 'no', 30, '/', '', '');}
 
 		alert('Saved');		 
 	});
 }
 
 
-heresay.getCookie = function (c_name)
+heresay.getCookie = function ( check_name ) {
+	// first we'll split this cookie up into name/value pairs
+	// note: document.cookie only returns name=value, not the other components
+	var a_all_cookies = document.cookie.split( ';' );
+	var a_temp_cookie = '';
+	var cookie_name = '';
+	var cookie_value = '';
+	var b_cookie_found = false; // set boolean t/f default f
+
+	for ( i = 0; i < a_all_cookies.length; i++ )
+	{
+		// now we'll split apart each name=value pair
+		a_temp_cookie = a_all_cookies[i].split( '=' );
+
+
+		// and trim left/right whitespace while we're at it
+		cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
+
+		// if the extracted name matches passed check_name
+		if ( cookie_name == check_name )
+		{
+			b_cookie_found = true;
+			// we need to handle case where cookie has no value but exists (no = sign, that is):
+			if ( a_temp_cookie.length > 1 )
+			{
+				cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+			}
+			// note that in cases where cookie is initialized but no value, null is returned
+			return cookie_value;
+			break;
+		}
+		a_temp_cookie = null;
+		cookie_name = '';
+	}
+	if ( !b_cookie_found )
+	{
+		return null;
+	}
+}
+	
+heresay.setCookie = function (name, value, expires, path, domain, secure )
 {
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-{
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
-    }
-  }
+	// set time, it's in milliseconds
+	var today = new Date();
+	today.setTime( today.getTime() );
+
+/*
+	if the expires variable is set, make the correct
+	expires time, the current script below will set
+	it for x number of days, to make it for hours,
+	delete * 24, for minutes, delete * 60 * 24
+*/
+	if ( expires )
+	{
+	expires = expires * 1000 * 60 * 60 * 24;
+	}
+	var expires_date = new Date( today.getTime() + (expires) );
+
+	document.cookie = name + "=" +escape( value ) +
+	( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
+	( ( path ) ? ";path=" + path : "" ) +
+	( ( domain ) ? ";domain=" + domain : "" ) +
+	( ( secure ) ? ";secure" : "" );
 }
 
-heresay.setCookie = function (c_name,value)
-{
-	var exdate=new Date();
-	var exdays = '600';
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie=c_name + "=" + c_value;
-}
 
