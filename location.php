7bbd9e6e2a30616dfa2ffdc31d7150bb7d2f5a79
jimmytidey <jimmytidey@gmail.com>
2011-10-23 11:39:41 +0100

    style for location.php

--- location.php
+++ location.php
@@ -1,10 +1,28 @@
 <?
-
-//this file is referenced through a mod rewrite rule
-
-	include('header.php'); 
+// MOD REWRITE !!!
+include('db_functions.php'); 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Heresay</title>
+
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+
+<script type="text/javascript">
+</script>
+
+<link rel=StyleSheet href="style/style.css" type="text/css" media='screen' />
+
+<head>
+
+<body>
+	
+
 
 <div id='container'>
 <?
