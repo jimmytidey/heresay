1f6923e8b0fb215e0bc53977d44aebda536f8739
jimmytidey <jimmytidey@gmail.com>
2011-10-24 09:42:41 +0100

    reducing icon size

--- platform/heresay_harringay.js
+++ platform/heresay_harringay.js
@@ -352,7 +352,7 @@ heresay.insertIcon = function(data, index) {
 	
 		// test to see if post has been located 
 	    if (data == 'no results found') {
-	        jQuery('.byline').eq(index).prepend("<div class='heresay_icon' style='" + icon_style + "' ><img src='"+heresay.baseURL+"/platform/images/heresay_location_button.jpg' class='garden_fence_icon' /><p style='" + icon_text_style + "'>Locate This Comment</p></div>");
+	        jQuery('.byline').eq(index).prepend("<div class='heresay_icon' style='" + icon_style + "' ><img src='"+heresay.baseURL+"/platform/images/heresay_icon.jpg' class='garden_fence_icon' /></div>");
 	    }
 
 	    else {
@@ -385,7 +385,7 @@ heresay.clickIcon = function(element, index) {
 
         var homeurl = location.pathname;
         var sub_page_id = heresay.findSubPageId(element);
-		var close_button_style = "font-size: 16px; left: 328px; position: absolute; top: 470px; width:100px; z-index: 15; height:26px;"; 
+		var close_button_style = "font-size: 16px; left: 328px; position: absolute; top: 462px; width:100px; z-index: 15; height:26px;"; 
 
         //add the modal window
         jQuery('body').append("<div id='garden_fence_modal' style='background-image: url("+heresay.baseURL+"/platform/images/modal_background.png);background-repeat:none; z-index:5;'><p><a id='garden_fence_close' style='float:right; z-index:20' href='#'><img src='"+heresay.baseURL+"/platform/images/cross.png' style='margin-right:20px; margin-top:20px; ' /> </a></p>	<iframe id='map_iframe' src='"+heresay.baseURL+"/platform/iframe.html?title=" + title + "&body_text=" + bodytext + "&home_url=" + homeurl + "&domain=" + domain + "&thread_date=" + thread_date + "&sub_page_id=" + sub_page_id + "&center="+heresay.homeCoords+"' frameborder='0' scrolling='vertical' style='height:485px; width:530px; margin: -10px 20px 0px 20px; z-index:10' ></iframe><input type='button' value='close' id='close_button_overlay' style='"+close_button_style+"' />  </div>");
