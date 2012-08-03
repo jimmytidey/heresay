81ada937fe5bb5220d5f8e3772a1916a8606d942
jimmytidey <jimmytidey@gmail.com>
2012-08-03 19:42:53 +0100

    automated commit

--- html/gather/se1.php
+++ html/gather/se1.php
@@ -1,20 +1,18 @@
 <?
-
-
 include('include.php');
+
+$read_url = 'http://www.london-se1.co.uk/forum/read/';
 
 $sites[0]['url']    = 'http://www.london-se1.co.uk/forum/rss/1';
 $sites[0]['name']   = 'se1';
 
 
-/*
-$feeds[1]['url']    = 'https://www.ictu.nl/rss.xml';
-$feeds[1]['none']   = '';
+$sites[1]['url']    = 'http://www.london-se1.co.uk/forum/rss/4';
+$sites[1]['name']   = 'se1';
 
-$feeds[2]['url']    = 'https://www.ictu.nl/rss.xml';
-$feeds[2]['none']   = '';
-*/
-    
+
+
+
 foreach($sites as $site) { 
     
     $feed = new SimplePie();
@@ -23,41 +21,68 @@ foreach($sites as $site) {
 	$feed->init();
 	$feed->handle_content_type();
 	
-
-	
 	$max = $feed->get_item_quantity();
 	for ($x = 0; $x < $max; $x++)  {
 		
 		$item = $feed->get_item($x);
-		
-		print_r($item);
-	
-        //test for uniqueness 
-        $query = "SELECT * FROM manual_updates WHERE link = '".$item->get_permalink()."'";
-        $result = mysql_fetch_array(mysql_query($query)); 
+	    
+        $link = $item->get_link();
+
+        $link = explode('/', $link);
+
+        $original_post_url = $read_url . "/" . $link[5] . "/" . $link[6];
+
+
+        $shit_html = file_get_contents($original_post_url); 
+
+        //get title 
+        $less_shit_html = explode('<h1>', $shit_html);
+        $title = explode('</h1>', $less_shit_html[1]); 
+        $title = strip_tags($title[0]);        
         
+        //get description
+        $less_shit_html = explode('<div class="PhorumReadBodyText">', $shit_html);
+        $description = explode('</div>', $less_shit_html[1]); 
+        $description = $description[0];        
+        
+	    echo "<h3>".$title.'</h3>';
+	    echo $description ;
+	   
+	    
+        $query = "SELECT * FROM manual_updates WHERE link = '".$original_post_url."'";
+        $result = mysql_fetch_array(mysql_query($query)); 
+    
         if (empty($result)) { 
-                     
+            
             $query = "INSERT INTO manual_updates (site, title, description, link, pubdate) 
             VALUES (
                 '".$site['name']."', 
-                '".mysql_real_escape_string($item->get_title())."', 
-                '".mysql_real_escape_string($item->get_description())."', 
-                '".mysql_real_escape_string($item->get_permalink())."', 
+                '".mysql_real_escape_string($title)."', 
+                '".mysql_real_escape_string($description)."', 
+                '".mysql_real_escape_string($original_post_url)."', 
                 '".mysql_real_escape_string(strtotime($item->get_date()))."')";       
-            echo $query; 
+            //echo $query; 
 
-            mysql_query($query); 
+          mysql_query($query); 
             ?>
-    		<div class="item">
-    			<h2 class="title"><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
-    			<?php echo $item->get_description(); ?>
-    			<p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
-    		</div>
+    	        <h3>SAVED</h3>
+    	        
             <?
         }   
+    
+        ?>
+        <br/>
+        <br/>
+        <br/>
+        <hr/>        
+        <?
+        
     }
 }
+
+
+
+?>
 
 
 
