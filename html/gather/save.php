7facd047beb5dc81b6bdd627a7167bca126e3790
jimmytidey <jimmytidey@gmail.com>
2012-08-03 18:44:00 +0100

    automated commit

---
+++ html/gather/save.php
@@ -0,0 +1,15 @@
+<?
+include('../db_info.php');
+
+$lat =  mysql_real_escape_string($_GET['lat']);
+$lng =  mysql_real_escape_string($_GET['lng']);
+$link = mysql_real_escape_string($_GET['link']);
+
+$query = "UPDATE manual_updates SET lat='$lat', lng='$lng' WHERE link='$link'";
+echo $query; 
+
+mysql_query ($query);
+
+
+
+?>
