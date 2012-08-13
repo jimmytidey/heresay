5a84d4a348d1e84c0e87dd0106b50119352cd82b
jimmytidey <jimmytidey@gmail.com>
2012-08-13 22:30:37 +0100

    automated commit

--- html/gather/locate.php
+++ html/gather/locate.php
@@ -195,6 +195,7 @@ $location_results    = db_q($location_query);
 
                                     <select id='category_<?=$i ?>_1'>
                                         <option value='--'>--</option>
+                                        <option value='Local_knowledge'>Local knowledge</option>
                                         <option value='forsale_giveaway'>Buy Sell</option>
                                         <option value='charity'>Charity</option>                            
                                         <option value='pets_nature'>Pets and nature</option>   
@@ -210,6 +211,7 @@ $location_results    = db_q($location_query);
 
                                     <select id='category_<?=$i ?>_2'>
                                         <option value='--'>--</option>
+                                        <option value='Local_knowledge'>Local knowledge</option>
                                         <option value='forsale_giveaway'>Buy Sell</option>
                                         <option value='charity'>Charity</option>                            
                                         <option value='pets_nature'>Pets and nature</option>   
@@ -225,6 +227,7 @@ $location_results    = db_q($location_query);
 
                                     <select id='category_<?=$i ?>_3'>
                                         <option value='--'>--</option>
+                                        <option value='Local_knowledge'>Local knowledge</option>
                                         <option value='forsale_giveaway'>Buy Sell</option>
                                         <option value='charity'>Charity</option>                            
                                         <option value='pets_nature'>Pets and nature</option>   
@@ -240,6 +243,7 @@ $location_results    = db_q($location_query);
                                     
                                     <select id='category_<?=$i ?>_4'>
                                         <option value='--'>--</option>
+                                        <option value='Local_knowledge'>Local knowledge</option>
                                         <option value='forsale_giveaway'>Buy Sell</option>
                                         <option value='charity'>Charity</option>                            
                                         <option value='pets_nature'>Pets and nature</option>   
