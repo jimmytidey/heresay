7e707a3690f668eb093b1f114627ef1ee5718730
jimmytidey <jimmytidey@gmail.com>
2012-08-16 00:06:52 +0100

    automated commit

--- html/api/recent_threads.php
+++ html/api/recent_threads.php
@@ -60,6 +60,8 @@ $search_result = db_q($query);
 if (empty($search_result)) { 
     $search_result['error'] = 'no results';
 }
+
+$search_result['query']= $query;
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
