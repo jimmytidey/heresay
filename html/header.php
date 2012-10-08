67a89e292b877ac91dd1f7702d72545870d31892
jimmytidey <jimmytidey@gmail.com>
2012-10-08 15:00:54 +0100

    adding google site verification

--- html/header.php
+++ html/header.php
@@ -16,8 +16,11 @@ $id = $id[$number];
 <head>
 <meta charset="utf-8">
 <title>Heresay</title>
+    <meta name="google-site-verification" content="ZuChEhlvuI1zxLGX7PYcfhNkF9ypHBqI8cGChpHyOZU" />
+    
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
     <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAOoFM_kDNJVs_SzvkytQRKBSpgYF3iu7PXc-1iPSD4CpffT2eCRRzD14PFyag3JY4SakJE5_wVYpLxw&sensor=false" type="text/javascript"></script>
+   
     <script type="text/javascript" src="scripts/mapstraction.js"></script>
     <link rel=StyleSheet href="/style/style.css" type="text/css" media='screen' />
     <script src='/scripts/heresay.js'></script>
