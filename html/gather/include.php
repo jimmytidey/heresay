af4f0183e6460e0442688905aeb9fe00a8c30736
jimmytidey <jimmytidey@gmail.com>
2012-10-13 14:28:43 +0100

    automated commit

--- html/gather/include.php
+++ html/gather/include.php
@@ -1,5 +1,5 @@
 <?
 include('../db_info.php');
 include_once('../simplepie/autoloader.php');
-include_once('../simplepie/idn/idna_convert.class.php');
+include_once('../simplepie/idn/idna_convert.class.php'); 
 ?>
