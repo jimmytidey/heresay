8b07c712d124f27d666933ecfbd5cb2b7584a4f4
jimmytidey <jimmytidey@gmail.com>
2012-10-13 14:42:53 +0100

    automated commit

--- db_info_example.php
+++
@@ -1,8 +0,0 @@
-<?
-$dbhost = '';
-$dbuser = '';
-$dbpass = '';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
-$dbname = '';
-mysql_select_db($dbname) or die ('Error connecting to mysql');
-?>
