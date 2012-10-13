ea6c2e74eed9682e06416f2983d400ff645e748b
jimmytidey <jimmytidey@gmail.com>
2012-10-13 13:55:44 +0100

    automated commit

---
+++ html/gather/simple_2.php
@@ -0,0 +1,95 @@
+<?
+include('include.php');
+
+
+$sites[3]['url']    = 'http://www.creativeclerkenwell.com/feed/atom/';
+$sites[3]['name']   = 'creativeclerkenwell.com';
+
+$sites[5]['url']    = 'http://www.westealingneighbours.org.uk/WEN-blog/feed/';
+$sites[5]['name']   = 'www.westealingneighbours.org.uk';
+
+$sites[6]['url']    = 'http://www.duchessofhackney.com/feed/';
+$sites[6]['name']   = 'duchessofhackney.com';
+
+$sites[7]['url']    = 'http://davehill.typepad.com/claptonian/rss.xml';
+$sites[7]['name']   = 'davehill.typepad.com/claptonian';
+
+$sites[40]['url']    = 'http://www.brixtonblog.com/feed';
+$sites[40]['name']   = 'brixtonblog.com';
+
+$sites[42]['url']    = 'http://www.vauxhallcivicsociety.org.uk/feed/';
+$sites[42]['name']   = 'vauxhallcivicsociety.org.uk';
+
+$sites[65]['url']    = 'http://feeds.feedburner.com/hackney-whats-on-children-young-people?format=xml';
+$sites[65]['name']   = 'feedburner.com/hackney-whats-on-children-young-people';
+
+$sites[66]['url']    = 'http://feeds.feedburner.com/hackney-whats-on-noticeboard';
+$sites[66]['name']   = 'feeds.feedburner.com/hackney-whats-on-noticeboard';
+
+$sites[67]['url']    = 'http://www.transitionstreatham.org/';
+$sites[67]['name']   = 'transitionstreatham.org';
+
+$sites[68]['url']    = 'http://www.wandsworthsw18.com/rss.xml';
+$sites[68]['name']   = 'wandsworthsw18.com';
+
+
+$sites[68]['url']    = 'http://www.queensparkforum.org/blog.php/feed/';
+$sites[68]['name']   = 'queensparkforum.org';
+
+
+
+    
+foreach($sites as $site) { 
+    
+    echo "<h1>".$site['name']."</h1>"; 
+    
+    $feed = new SimplePie();
+	$feed->set_feed_url($site['url']);
+	$feed->enable_cache(false);
+	$feed->init();
+	$feed->handle_content_type();
+	
+	$max = $feed->get_item_quantity();
+	for ($x = 0; $x < $max; $x++)  {
+		
+		$item = $feed->get_item($x);
+	    
+	    echo "<h3>".strip_tags($item->get_title()). "<em>" .$item->get_date() ."</em></h3>"; 
+        echo "<p>". strip_tags($item->get_description()). "</p>";
+        //test for uniqueness 
+        $query = "SELECT * FROM manual_updates WHERE link = '".$item->get_permalink()."'";
+        echo $query; 
+        $result = mysql_fetch_array(mysql_query($query)); 
+        
+        if (empty($result)) { 
+            
+            echo "<p style='color:red'>SAVING</p>"; 
+            
+            $query = "INSERT INTO manual_updates (site, title, description, link, pubdate) 
+            VALUES (
+                '".$site['name']."', 
+                '".mysql_real_escape_string(strip_tags($item->get_title()))."', 
+                '".mysql_real_escape_string(strip_tags($item->get_description()))."', 
+                '".mysql_real_escape_string($item->get_permalink())."', 
+                '".mysql_real_escape_string(strtotime($item->get_date()))."')";       
+
+
+            mysql_query($query); 
+
+        } 
+        
+        else { 
+            echo "<p style='color:green'>Already in</p><br/>"; 
+        }  
+    }
+    
+    echo "<hr />";
+}
+
+
+
+
+
+
+
+?>
