bcd826e8b2063fc75c208c36ed0d0f23e2d9d289
jimmytidey <jimmytidey@gmail.com>
2012-10-14 22:30:00 +0100

    automated commit

--- html/gather/simple_2.php
+++ html/gather/simple_2.php
@@ -2,7 +2,7 @@
 include('include.php');
 
 
-$sites[3]['url']    = 'http://creativeclerkenwell.com/feed/rss/atom';
+$sites[3]['url']    = 'http://creativeclerkenwell.com/feed/rss';
 $sites[3]['name']   = 'creativeclerkenwell.com';
 
 $sites[5]['url']    = 'http://westealingneighbours.org.uk/WEN-blog/feed/';
@@ -34,63 +34,46 @@ $sites[68]['name']   = 'queensparkforum.org';
     
 foreach($sites as $site) { 
     echo "<h1>".$site['url']."</h1>";
-    
-     $feed = new SimplePie();
-    $feed->force_feed(true);
-     
-	$feed->set_feed_url($site['url']);
-
-    //enable caching
-    $feed->enable_cache(false);
-
-
-    //set the amount of seconds you want to cache the feed
-    $feed->set_cache_duration(1800);
-
-    //init the process
-    $feed->init();
-
-    //let simplepie handle the content type (atom, RSS...)
-    $feed->handle_content_type();
-    
-  
-	$max = $feed->get_item_quantity();
+    $feedUrl = $site['url'];
+    $rawFeed = file_get_contents($feedUrl);
+    $xml = new SimpleXmlElement($rawFeed);
 	
-	echo "<h1>".$site['name'] . " - " . $max ."</h1>"; 
-	
-	for ($x = 0; $x < $max; $x++)  {
-		
-		$item = $feed->get_item($x);
-	    
-	    echo "<h3>".strip_tags($item->get_title()). "<em>" .$item->get_date() ."</em></h3>"; 
-        echo "<p>". strip_tags($item->get_description()). "</p>";
-        
-        //test for uniqueness 
-        $query = "SELECT * FROM manual_updates WHERE link = '".$item->get_permalink()."'";
-        echo $query; 
-        $result = mysql_fetch_array(mysql_query($query)); 
-        
-        if (empty($result)) { 
-            
-            echo "<p style='color:red'>SAVING</p>"; 
-            
-            $query = "INSERT INTO manual_updates (site, title, description, link, pubdate) 
-            VALUES (
-                '".$site['name']."', 
-                '".mysql_real_escape_string(strip_tags($item->get_title()))."', 
-                '".mysql_real_escape_string(strip_tags($item->get_description()))."', 
-                '".mysql_real_escape_string($item->get_permalink())."', 
-                '".mysql_real_escape_string(strtotime($item->get_date()))."')";       
+	echo "<h1>".$site['name'] . "</h1>"; 
+    foreach ($xml->channel->item as $item)
+    {
+    	    
+    	    
+    	    echo "<h3>".strip_tags($item->title). "<em>" .$item->pubDate ."</em></h3>"; 
+            echo "<p>". strip_tags($item->description). "</p>";
+          
+            //test for uniqueness 
+            $query = "SELECT * FROM manual_updates WHERE title = '".$item->title."' && site= '" .$site['name']."' ";
+            echo $query; 
+            $result = mysql_fetch_array(mysql_query($query)); 
+
+            if (empty($result)) { 
+
+              echo "<p style='color:red'>SAVING</p>"; 
+
+              $query = "INSERT INTO manual_updates (site, title, description, link, pubdate) 
+              VALUES (
+                  '".$site['name']."', 
+                  '".mysql_real_escape_string(strip_tags($item->title))."', 
+                  '".mysql_real_escape_string(strip_tags($item->description))."', 
+                  '".mysql_real_escape_string($item->guid)."', 
+                  '".mysql_real_escape_string(strtotime($item->pubDate))."')";       
 
 
-            mysql_query($query); 
+              mysql_query($query); 
 
-        } 
-        
-        else { 
-            echo "<p style='color:green'>Already in</p><br/>"; 
-        }  
+            } 
+
+            else { 
+              echo "<p style='color:green'>Already in</p><br/>"; 
+            }
     }
+	    
+
     
     echo "<hr />";
 }
