9d0cf0aa612b8acdd9918b0eee194d1c9c41b7da
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-14 15:10:20 +0000

    automated commit

--- html/footer.php
+++ html/footer.php
@@ -11,7 +11,16 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        
+        <!--[if lte IE 9]>
+             <script src="js/main_ie.js"></script>
+        <![endif]-->
+        
+        <!--[if !IE]> -->
+         <  script src="js/main.js"></script>
+        <!-- <![endif]-->
+        
+       
         
         
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
