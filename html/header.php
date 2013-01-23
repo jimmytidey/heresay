ece603b0769628a237ea8daa5811abb3d3df0d7f
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-23 09:59:13 +0000

    automated commit

--- html/header.php
+++ html/header.php
@@ -16,16 +16,18 @@ $db = new dbClass(DB_LOCATION, DB_USER_NAME, DB_PASSWORD, DB_NAME);
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
-        <script type="text/javascript" src="http://fast.fonts.com/jsapi/3b8a8020-e2d5-4375-8d0a-fcf451d4b03a.js"></script>
+
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
         <link rel="stylesheet" href="css/base.css">
         <link rel="stylesheet" href="css/layout.css">
         <link rel="stylesheet" href="css/skeleton.css">
         <link rel="stylesheet" href="css/main.css">
- 
+        <link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/3b8a8020-e2d5-4375-8d0a-fcf451d4b03a.css"/>        
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false"></script>
+        
         <script src="js/vendor/modernizr-2.6.1.min.js"></script>
-    </head>
+        <script type="text/javascript" src="http://fast.fonts.com/jsapi/3b8a8020-e2d5-4375-8d0a-fcf451d4b03a.js"></script>    </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
@@ -43,7 +45,7 @@ $db = new dbClass(DB_LOCATION, DB_USER_NAME, DB_PASSWORD, DB_NAME);
                     
                 </div>
                 <div class='two-thirds column omega'>
-                    <h2>Heresay gathers locally-focused comment from across the web and presents it in one searchable place.</h2>
+                    <h2>Heresay gathers locally-focused comment from across the web and presents it in one ssearchable place.</h2>
                 </div>
                 </div>
             </div>
