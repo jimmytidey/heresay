e19702d3a45561f6615c9d1a1192e142d00cf650
Jimmy Tidey <jimmytidey@gmail.com>
2013-05-09 12:54:35 +0100

    add favicon, tidy up

--- html/functions/includes.php
+++ html/functions/includes.php
@@ -2,16 +2,9 @@
 //set timezone
 date_default_timezone_set ('Europe/London');
 
-//phpQuery;
-require_once __DIR__ .'/../classes/phpQuery.php';
 
 //Instantiate the DB class, constructor will connect to the DB
 require_once  __DIR__ .'/../classes/dbClass.php';
-
-
-//Report and People use classes that extent this class
-require_once __DIR__ ."/../classes/scraperBaseClass.php";
-
 
 require_once  __DIR__ .'/json.php';
 require_once  __DIR__ .'/url_clean.php';
